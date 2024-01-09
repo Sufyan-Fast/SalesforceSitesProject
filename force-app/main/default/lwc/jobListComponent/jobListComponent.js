96f0f74e13acbb9aeccc05dd08423057dde4c355
Sufyan Ashraf <sufyanashraf5469@gmail.com>
2024-01-09 19:44:49 +0500

    full latest

---
+++ force-app/main/default/lwc/jobListComponent/jobListComponent.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/images';
+
+export default class JobListComponent extends LightningElement {
+
+
+    teamsImage = IMAGES + '/images/MicrosoftTeams-image (23).png';
+}
